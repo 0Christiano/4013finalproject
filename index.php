49e0d393f5a9ef7e0894368be1bf18fe252026e1
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 09:20:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,3 +6,21 @@ include "view-header.php";
   <?php
       include "view-footer.php";
 ?>
+
+
+<?php
+session_start();
+include('functions.php');
+
+// Get items from the database
+$items = getItems();
+?>
+
+<!-- Display items -->
+<h1>Items</h1>
+<?php
+while ($row = $items->fetch_assoc()) {
+    echo "<p>{$row['name']} - {$row['description']} 
+    <a href='add_to_cart.php?item_id={$row['item_id']}'>Add to Cart</a></p>";
+}
+?>
