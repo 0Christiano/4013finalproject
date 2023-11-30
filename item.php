357f7a98a00b89cfea974fa33d0a1be07f048fdd
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 09:26:40 -0600

    Update item.php

--- item.php
+++ item.php
@@ -1,10 +1,34 @@
 <?php
-require_once("util-db.php");
-require_once("model-fighter.php");
+session_start();
+include('functions.php');
 
-$pageTitle = "Fighter";
-include "view-header.php";
-$fighters = selectfighters();
-include "view-fighter.php";
-      include "view-footer.php";
+if (isset($_GET['item_id'])) {
+    $itemId = $_GET['item_id'];
+
+    // Retrieve item details from the database based on $itemId
+    global $conn;
+    $sql = "SELECT * FROM Item WHERE item_id = $itemId";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $item = $result->fetch_assoc();
+    } else {
+        echo "Item not found.";
+        exit();
+    }
+} else {
+    echo "Item ID not provided.";
+    exit();
+}
 ?>
+
+<!-- Display item details -->
+<h1><?php echo $item['item_name']; ?></h1>
+<p>Price: <?php echo $item['other_details']; ?></p>
+<!-- Display other details as needed -->
+
+<!-- Add to Cart button -->
+<form action="add_to_cart.php" method="GET">
+    <input type="hidden" name="item_id" value="<?php echo $item['item_id']; ?>">
+    <input type="submit" value="Add to Cart">
+</form>
