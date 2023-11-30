e01c56b48ec72cecf25f2129eb19244de504cc91
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 13:38:18 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // Include the necessary files or define global functions and variables
-include('util-db.php'); // Include the file containing get_db_connection() function
+include_once('util-db.php'); // Include the file containing get_db_connection() function
 
+// Function to get items from the database
 if (!function_exists('getItems')) {
-    // Function to get items from the database
     function getItems() {
         $conn = get_db_connection(); // Get the database connection
         
@@ -25,40 +25,48 @@ if (!function_exists('getItems')) {
 }
 
 // Function to get cart items
-function getCartItems() {
-    if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
-        return []; // Return an empty array if cart is empty or not set
-    } else {
-        return $_SESSION['cart']; // Return cart items from session
+if (!function_exists('getCartItems')) {
+    function getCartItems() {
+        if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
+            return []; // Return an empty array if cart is empty or not set
+        } else {
+            return $_SESSION['cart']; // Return cart items from session
+        }
     }
 }
 
 // Function to add an item to the cart
-function addToCart($itemId) {
-    // Your logic to add the item to the cart (using session or database)
-    // Example: adding $itemId to the $_SESSION['cart'] array
-    $_SESSION['cart'][] = $itemId;
+if (!function_exists('addToCart')) {
+    function addToCart($itemId) {
+        // Your logic to add the item to the cart (using session or database)
+        // Example: adding $itemId to the $_SESSION['cart'] array
+        $_SESSION['cart'][] = $itemId;
+    }
 }
 
 // Function to get item details based on item ID
-function getItemDetails($itemID) {
-    $conn = get_db_connection(); // Get the database connection
-    
-    if ($conn === false) {
-        return false; // Return false if the connection fails
+if (!function_exists('getItemDetails')) {
+    function getItemDetails($itemID) {
+        $conn = get_db_connection(); // Get the database connection
+        
+        if ($conn === false) {
+            return false; // Return false if the connection fails
+        }
+
+        // Your SQL query to retrieve item details based on $itemID
+        $sql = "SELECT item_ID, name, description FROM Item WHERE item_ID = $itemID";
+
+        $result = $conn->query($sql); // Execute the query
+
+        if (!$result || $result->num_rows == 0) {
+            return false; // Return false if the query fails or no item found
+        }
+
+        $itemDetails = $result->fetch_assoc(); // Fetch item details
+
+        return $itemDetails; // Return the item details as an associative array
     }
-
-    // Your SQL query to retrieve item details based on $itemID
-    $sql = "SELECT item_ID, name, description FROM Item WHERE item_ID = $itemID";
-
-    $result = $conn->query($sql); // Execute the query
-
-    if (!$result || $result->num_rows == 0) {
-        return false; // Return false if the query fails or no item found
-    }
-
-    $itemDetails = $result->fetch_assoc(); // Fetch item details
-
-    return $itemDetails; // Return the item details as an associative array
 }
+
+// Other functions...
 ?>
