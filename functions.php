c8df84c3bf209f29fa9eb3b67cdb6f3a590c356c
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 13:27:28 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,38 +1,43 @@
 <?php
+// Include the necessary files or define global functions and variables
 include('util-db.php'); // Include the file containing get_db_connection() function
 
 // Function to get items from the database
 function getItems() {
     $conn = get_db_connection(); // Get the database connection
     
+    // Your existing getItems() function logic...
+}
+
+// Function to get cart items
+function getCartItems() {
+    // Your existing getCartItems() function logic...
+}
+
+// Function to add an item to the cart
+function addToCart($itemId) {
+    // Your existing addToCart() function logic...
+}
+
+// Function to get item details based on item ID
+function getItemDetails($itemID) {
+    $conn = get_db_connection(); // Get the database connection
+    
     if ($conn === false) {
         return false; // Return false if the connection fails
     }
 
-    // Your SQL query to retrieve items (adjust column names as needed)
-    $sql = "SELECT item_ID, name, description FROM Item";
+    // Your SQL query to retrieve item details based on $itemID
+    $sql = "SELECT item_ID, name, description FROM Item WHERE item_ID = $itemID";
 
     $result = $conn->query($sql); // Execute the query
 
-    if (!$result) {
-        return false; // Return false if the query fails
+    if (!$result || $result->num_rows == 0) {
+        return false; // Return false if the query fails or no item found
     }
 
-    return $result; // Return the query result (should be a MySQLi result object)
-}
-function getCartItems() {
-    if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
-        return []; // Return an empty array if cart is empty or not set
-    } else {
-        return $_SESSION['cart']; // Return cart items from session
-    }
-}
+    $itemDetails = $result->fetch_assoc(); // Fetch item details
 
-function addToCart($itemId) {
-    // Your logic to add the item to the cart (using session or database)
-    // Example: adding $itemId to the $_SESSION['cart'] array
-    $_SESSION['cart'][] = $itemId;
+    return $itemDetails; // Return the item details as an associative array
 }
-
-
 ?>
