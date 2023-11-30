3a41c1b9c500583f1725bed12f81e1d711c0191b
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 09:32:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
+
 $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>4013 Final Project</h1>
   <?php
       include "view-footer.php";
-?>
 
 
-<?php
 session_start();
 include('functions.php');
 
