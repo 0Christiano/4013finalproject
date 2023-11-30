71ea2d861bb3aada85eb2e9be37ad9852496ce27
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 13:12:53 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,26 +1,28 @@
 <?php
 session_start();
-include('functions.php');
 
-//get cart items
-$cartItems = getCartItems();
-?>
-
-<!-- display cart items -->
-<h1>Cart</h1>
-<?php
-if (!empty($cartItems)) {
-    foreach ($cartItems as $item_id) {
-        
-        $itemDetails = getItemDetails($$item_id);
-
-        echo "<p>Item ID: " . $itemDetails['id'] . "</p>";
-        echo "<p>Item Name: " . $itemDetails['name'] . "</p>";
-        echo "<p>Item Price: " . $itemDetails['price'] . "</p>";
-
-       echo "<hr>";
-    }
-} else {
+if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
     echo "<p>Your cart is empty.</p>";
+} else {
+    // Include necessary files/functions
+    include('functions.php');
+
+    // Get cart items
+    $cartItems = getCartItems(); // You'll need to create this function in functions.php
+
+    if (!empty($cartItems)) {
+        foreach ($cartItems as $itemID) {
+            // Get item details based on $itemID (function to fetch item details)
+            $itemDetails = getItemDetails($itemID); // You'll need to create this function in functions.php
+
+            // Display item details
+            echo "<p>Item ID: " . $itemDetails['item_ID'] . "</p>";
+            echo "<p>Item Name: " . $itemDetails['name'] . "</p>";
+            echo "<p>Item Description: " . $itemDetails['description'] . "</p>";
+            echo "<hr>";
+        }
+    } else {
+        echo "<p>Your cart is empty.</p>";
+    }
 }
 ?>
