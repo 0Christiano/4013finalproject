a11b7f144d83b42c40c6f86ae786548a41ccef34
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 09:37:50 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,27 @@
 <?php
-// db connection
-include('util-db.php');
+include('util-db.php'); // Include the file containing your database connection function
 
-// gets items from db
+// Function to get database connection
+function get_db_connection(){
+    $conn = get_db_connection(); // Replace this with your actual function to establish a database connection
+    return $conn;
+}
+
+// Function to get items from the database
 function getItems() {
-    global $conn;
-    $sql = "SELECT * FROM Item";
-    $result = $conn->query($sql);
-    return $result;
-}
+    $conn = get_db_connection(); // Get the database connection
+    
+    if ($conn === false) {
+        return false; // Return false if the connection fails
+    }
 
-// add items to cart
-function addToCart($item_id) {
-    // Example: $_SESSION['cart'][] = $itemId;
-}
+    $sql = "SELECT * FROM Item"; // Your SQL query to retrieve items
+    $result = $conn->query($sql); // Execute the query
 
-// get the cart items
-function getCartItems() { 
-    // Example: return $_SESSION['cart'];
+    if (!$result) {
+        return false; // Return false if the query fails
+    }
+
+    return $result; // Return the query result (should be a MySQLi result object)
 }
 ?>
