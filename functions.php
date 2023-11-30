70a6d8c70bc1fa228a046a4bbda8c9df661cc9e5
Christian <142837529+0Christiano@users.noreply.github.com>
2023-11-30 13:32:00 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,24 +2,26 @@
 // Include the necessary files or define global functions and variables
 include('util-db.php'); // Include the file containing get_db_connection() function
 
-// Function to get items from the database
-function getItems() {
-    $conn = get_db_connection(); // Get the database connection
-    
-    if ($conn === false) {
-        return false; // Return false if the connection fails
+if (!function_exists('getItems')) {
+    // Function to get items from the database
+    function getItems() {
+        $conn = get_db_connection(); // Get the database connection
+        
+        if ($conn === false) {
+            return false; // Return false if the connection fails
+        }
+
+        // Your SQL query to retrieve items (adjust column names as needed)
+        $sql = "SELECT item_ID, name, description FROM Item";
+
+        $result = $conn->query($sql); // Execute the query
+
+        if (!$result) {
+            return false; // Return false if the query fails
+        }
+
+        return $result; // Return the query result (should be a MySQLi result object)
     }
-
-    // Your SQL query to retrieve items (adjust column names as needed)
-    $sql = "SELECT item_ID, name, description FROM Item";
-
-    $result = $conn->query($sql); // Execute the query
-
-    if (!$result) {
-        return false; // Return false if the query fails
-    }
-
-    return $result; // Return the query result (should be a MySQLi result object)
 }
 
 // Function to get cart items
