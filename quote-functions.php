070d88ac235b639c9b450f6dc6403e4d8bd5124e
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-04 15:15:46 -0600

    Update quote-functions.php

--- quote-functions.php
+++ quote-functions.php
@@ -1,20 +1,23 @@
-
 <?php
 include_once('util-db.php'); // Include the file containing get_db_connection() function
 
 // Function to save user information into the Quote table
-function saveQuote($customerInfo) {
+function saveQuote($id, $name, $address, $creditCard) {
     $conn = get_db_connection(); // Get the database connection
     
     if ($conn === false) {
         return false; // Return false if the connection fails
     }
 
-    // Escape the customerInfo string to prevent SQL injection
-    $escapedCustomerInfo = mysqli_real_escape_string($conn, $customerInfo);
+    // Escape the variables to prevent SQL injection
+    $safeId = mysqli_real_escape_string($conn, $id);
+    $safeName = mysqli_real_escape_string($conn, $name);
+    $safeAddress = mysqli_real_escape_string($conn, $address);
+    $safeCreditCard = mysqli_real_escape_string($conn, $creditCard);
 
     // SQL query to insert customer information into the Quote table
-    $sql = "INSERT INTO Quote (statement) VALUES ('$escapedCustomerInfo')";
+    $sql = "INSERT INTO Quote (ID, name, address, credit_card) 
+            VALUES ('$safeId', '$safeName', '$safeAddress', '$safeCreditCard')";
 
     if ($conn->query($sql) === TRUE) {
         return $conn->insert_id; // Return the generated Quote ID
