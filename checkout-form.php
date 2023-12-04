8b21aadf8cb7b97f14114709363900591f27321a
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-04 15:31:42 -0600

    Update checkout-form.php

--- checkout-form.php
+++ checkout-form.php
@@ -1,6 +1,7 @@
 <!-- checkout-form.php -->
 <form method="POST" action="checkout.php">
     <!-- Customer information fields -->
+    <input type="text" name="id" placeholder="ID">
     <input type="text" name="name" placeholder="Name">
     <input type="text" name="address" placeholder="Address">
     <input type="text" name="credit_card" placeholder="Credit Card">
