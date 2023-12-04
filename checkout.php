c5fdf534e7046e25d02cf412f33b4339c9daa607
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-04 13:44:31 -0600

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,1 +1,28 @@
 
+<?php
+session_start();
+include('functions.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle form submission
+    $customerInfo = $_POST['customer_info']; // Assuming a field named 'customer_info' contains all customer details
+
+    // Save user information to the Quote table
+    $quoteId = saveQuote($customerInfo);
+
+    // Get cart items
+    $cartItems = getCartItems();
+
+    // Save cart items to the Quote_Item table
+    foreach ($cartItems as $itemId) {
+        saveQuoteItem($quoteId, $itemId);
+    }
+
+    // Clear the user's cart after checkout
+    clearCart();
+
+    // Redirect or display a success message
+    header('Location: success.php');
+    exit();
+}
+?>
