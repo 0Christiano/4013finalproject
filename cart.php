426556967545dbec7bcfb9b97cf34a985bbb5da3
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-04 22:10:14 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,31 +4,28 @@ include('functions.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['remove_item'])) {
         $itemId = $_POST['item_id'];
-        removeFromCart($itemId); // You need to create this function in functions.php
+        removeFromCart($itemId); 
     } elseif (isset($_POST['proceed_to_checkout'])) {
-        // Retrieve user information from the form
-        $id = $_POST['id']; // Assuming you have an 'id' field in your form
+        
+        $id = $_POST['id']; 
         $name = $_POST['name'];
         $address = $_POST['address'];
         $creditCard = $_POST['credit_card'];
 
-        // Validate and sanitize user inputs
-        // You should implement proper validation and sanitization here
-
-        // Insert user information into the "Quote" table
-        $quoteId = saveQuote($id, $name, $address, $creditCard); // You'll need to create this function in functions.php
+        // inserts the user input into the quote table of db
+        $quoteId = saveQuote($id, $name, $address, $creditCard); // functions.php
 
         if ($quoteId) {
-            // Get cart items
-            $cartItems = getCartItems(); // You'll need to create this function in functions.php
+            // get cart items
+            $cartItems = getCartItems(); // functions.php
 
-            // Insert cart items into the "Quote" table, associating them with the quote
-            saveCartQuote($quoteId, $cartItems); // You'll need to create this function in functions.php
+            // cart items into quote
+            saveCartQuote($quoteId, $cartItems); // in review??
 
-            // Clear the cart after successful checkout
+            // clear the cart after checkout
             clearCart();
 
-            // Display a success message or redirect to a thank you page
+            // success message WIP
             echo "<p>Checkout successful! Thank you for your purchase.</p>";
         } else {
             echo "<p>Failed to save user information into the statement table.</p>";
@@ -39,18 +36,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
     echo "<p>Your cart is empty.</p>";
 } else {
-    // Include necessary files/functions
+
     include('functions.php');
 
-    // Get cart items
-    $cartItems = getCartItems(); // You'll need to create this function in functions.php
+    // get cart items
+    $cartItems = getCartItems(); // functions.php
 
     if (!empty($cartItems)) {
         foreach ($cartItems as $itemID) {
-            // Get item details based on $itemID (function to fetch item details)
-            $itemDetails = getItemDetails($itemID); // You'll need to create this function in functions.php
+            
+            $itemDetails = getItemDetails($itemID); // functions.php
 
-            // Display item details
+            // item details
             echo "<p>Item ID: " . $itemDetails['item_ID'] . "</p>";
             echo "<p>Item Name: " . $itemDetails['name'] . "</p>";
             echo "<p>Item Description: " . $itemDetails['description'] . "</p>";
@@ -73,7 +70,7 @@ if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remove_item'])) {
     $itemId = $_POST['item_id'];
-    removeFromCart($itemId); // You need to create this function in functions.php
+    removeFromCart($itemId); // functions.php
   
 }
       echo "<p><a href='index.php'>Back to Home</a></p>";
