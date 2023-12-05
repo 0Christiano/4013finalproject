2c5ad3a00ee89e363e497362d15b02cef888b4b9
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-04 22:07:35 -0600

    Update add-to-cart.php

--- add-to-cart.php
+++ add-to-cart.php
@@ -2,15 +2,11 @@
 session_start();
 include('functions.php');
 
-// Check if item_id is received from the request
 if (isset($_GET['item_id'])) {
     $itemId = $_GET['item_id'];
-    addToCart($itemId); // Use $itemId instead of $item_id
+    addToCart($itemId); 
      
     header('Location: cart.php');
     exit();
 }
-
-// Redirect back to the index page or cart page
-// Example: header('Location: index.php');
 ?>
