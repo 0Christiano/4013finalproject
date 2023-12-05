521288d6ff5e1725bdf97af42bd60af0942c2ed7
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-05 15:50:09 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,9 +1,7 @@
 <?php
 function get_db_connection(){
-    // Create connection
     $conn = new mysqli('159.89.47.44', 'christi9_4013finalprojectuser', 'B?Yu~2BivfSZ', 'christi9_4013finalproject');
     
-    // Check connection
     if ($conn->connect_error) {
       return false;
     }
