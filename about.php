8d8e6ff36ed1c501109c3cc78e5a38a8a67caaf0
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-06 15:15:22 -0600

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us - Your Company Name</title>
+    <title>About Us - CJ's MarketPlace</title>
     <!-- Add your custom stylesheets or link to a CSS file here -->
     <style>
         body {
@@ -49,7 +49,7 @@
 <body>
 
     <header>
-        <h1>Your Company Name</h1>
+        <h1>CJ's MarketPlace</h1>
         <p>About Us</p>
     </header>
 
