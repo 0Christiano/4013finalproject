c10119d9923ed7b96d051566a0f0b26b23ef7c23
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-06 15:03:27 -0600

    Update about.php

--- about.php
+++ about.php
@@ -1,1 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - Your Company Name</title>
+    <!-- Add your custom stylesheets or link to a CSS file here -->
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            background-color: #f5f5f5;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em 0;
+        }
+
+        nav {
+            background-color: #444;
+            padding: 1em;
+        }
+
+        nav a {
+            color: #fff;
+            text-decoration: none;
+            margin: 0 1em;
+        }
+
+        main {
+            padding: 2em;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <header>
+        <h1>Your Company Name</h1>
+        <p>About Us</p>
+    </header>
+
+    <nav>
+        <!-- Add navigation links if needed -->
+        <a href="#home">Home</a>
+        <a href="#products">Products</a>
+        <a href="#contact">Contact</a>
+    </nav>
+
+    <main>
+        <section>
+            <h2>Welcome to [Your Company Name]</h2>
+            <p>We are passionate about [brief description of what your company does]. At [Your Company Name], we strive
+                to...</p>
+        </section>
+
+        <section>
+            <h2>Our Mission</h2>
+            <p>Our mission is to [insert your mission statement or purpose here].</p>
+        </section>
+
+        <section>
+            <h2>Key Features</h2>
+            <ul>
+                <li><strong>Feature 1:</strong> [Description of Feature 1]</li>
+                <li><strong>Feature 2:</strong> [Description of Feature 2]</li>
+                <li><strong>Feature 3:</strong> [Description of Feature 3]</li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>How to Contact Us</h2>
+            <p>For inquiries or more information, you can contact us at <a href="mailto:your.email@example.com">your.email@example.com</a>.</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 [Your Company Name]. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
 
