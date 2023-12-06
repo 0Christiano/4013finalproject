50b14510449ff6292889a7afa8ce95a98277858e
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-05 20:21:09 -0600

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -3,7 +3,7 @@ session_start();
 include('quote-functions.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit_checkout'])) {
-    // Get customer information from the form
+    // customer information we are getting from the form 
     $id = $_POST['id'];
     $name = $_POST['name'];
     $address = $_POST['address'];
@@ -11,32 +11,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit_checkout'])) {
 
     // Validate and sanitize user inputs (add your validation/sanitization logic here)
 
-    // Concatenate user information into a single string
+    // concatenate user information into a single string
     $customerInfo = "ID: $id, Name: $name, Address: $address, Credit Card: $creditCard";
 
-    // Save user information to the Quote table
+    // save to the quote table
     $quoteId = saveQuote($id, $name, $address, $creditCard);
 
     if ($quoteId) {
         // Get cart items
         $cartItems = getCartItems();
 
-        // Save cart items to the Quote_Item table
+        // save cart items to the Quote_Item table
         foreach ($cartItems as $itemId) {
             saveQuoteItem($quoteId, $itemId);
         }
 
-        // Clear the user's cart after checkout
+        // clear cart after checkout 
         clearCart();
 
-        // Redirect or display a success message
+        //redirect to index(home)
         header('Location: index.php');
         exit();
     } else {
         echo "<p>Failed to save user information into the statement table.</p>";
     }
 } else {
-    // Redirect to the checkout form if accessed without proper submission
+    // redirect to the checkout form 
     header('Location: checkout-form.php');
     exit();
 }
