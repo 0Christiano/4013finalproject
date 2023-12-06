de219910c4905cd2a3d819ff3e11a7caf175cd46
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-06 15:05:19 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,78 +1,49 @@
 <?php
 session_start();
 include('functions.php');
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['remove_item'])) {
         $itemId = $_POST['item_id'];
-        removeFromCart($itemId); 
+        removeFromCart($itemId);
     } elseif (isset($_POST['proceed_to_checkout'])) {
-        
-        $id = $_POST['id']; 
-        $name = $_POST['name'];
-        $address = $_POST['address'];
-        $creditCard = $_POST['credit_card'];
-
-        // inserts the user input into the quote table of db
-        $quoteId = saveQuote($id, $name, $address, $creditCard); // functions.php
-
-        if ($quoteId) {
-            // get cart items
-            $cartItems = getCartItems(); // functions.php
-
-            // cart items into quote
-            saveCartQuote($quoteId, $cartItems); // in review??
-
-            // clear the cart after checkout
-            clearCart();
-
-            // success message WIP
-            echo "<p>Checkout successful! Thank you for your purchase.</p>";
-        } else {
-            echo "<p>Failed to save user information into the statement table.</p>";
-        }
+        // ... (your existing code for checkout)
     }
 }
 
+// Check if the cart is empty
 if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
     echo "<p>Your cart is empty.</p>";
 } else {
-
-    include('functions.php');
-
-    // get cart items
-    $cartItems = getCartItems(); // functions.php
+    // Get cart items
+    $cartItems = getCartItems();
 
     if (!empty($cartItems)) {
+        echo "<div class='cart-items'>";
         foreach ($cartItems as $itemID) {
-            
-            $itemDetails = getItemDetails($itemID); // functions.php
+            $itemDetails = getItemDetails($itemID);
 
-            // item details
-            echo "<p>Item ID: " . $itemDetails['item_ID'] . "</p>";
-            echo "<p>Item Name: " . $itemDetails['name'] . "</p>";
-            echo "<p>Item Description: " . $itemDetails['description'] . "</p>";
-            echo "<hr>";
-
-             echo "<form method='post' action='cart.php'>";
+            // Display item details
+            echo "<div class='cart-item'>";
+            echo "<p><strong>Item ID:</strong> " . $itemDetails['item_ID'] . "</p>";
+            echo "<p><strong>Item Name:</strong> " . $itemDetails['name'] . "</p>";
+            echo "<p><strong>Description:</strong> " . $itemDetails['description'] . "</p>";
+            echo "<form method='post' action='cart.php'>";
             echo "<input type='hidden' name='item_id' value='$itemID'>";
             echo "<input type='submit' name='remove_item' value='Remove'>";
             echo "</form>";
-
-            echo "<hr>";
+            echo "</div>";
         }
-        echo "<form method='post' action='checkout.php'>";
+        echo "</div>";
+
+        // Proceed to checkout button
+        echo "<form class='checkout-form' method='post' action='checkout.php'>";
         echo "<input type='submit' name='proceed_to_checkout' value='Proceed to Checkout'>";
         echo "</form>";
-
     } else {
         echo "<p>Your cart is empty.</p>";
     }
 }
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['remove_item'])) {
-    $itemId = $_POST['item_id'];
-    removeFromCart($itemId); // functions.php
-  
-}
-      echo "<p><a href='index.php'>Back to Home</a></p>";
-?>
 
+echo "<p><a href='index.php'>Back to Home</a></p>";
+?>
