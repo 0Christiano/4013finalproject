37a46a5a1c86d530771c67461bb5cfc64282b7fd
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-06 14:30:36 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -43,9 +43,9 @@
   </style>
 </head>
 <body>
-  <div class="container">
+  <div class="container-fluid"> <!-- Use container-fluid for a full-width container -->
     <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
+      <div class="container">
         <a class="navbar-brand" href="#">Your Brand</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
