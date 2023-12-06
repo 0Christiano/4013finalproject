da4a34e454c1d0329943054dafdb988973a57fd4
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-05 20:30:14 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,30 +1,29 @@
 <?php
-// Include the necessary files or define global functions and variables
 include_once('util-db.php'); // Include the file containing get_db_connection() function
 
-// Function to get items from the database
+// get items from the database
 if (!function_exists('getItems')) {
     function getItems() {
         $conn = get_db_connection(); // Get the database connection
 
         if ($conn === false) {
-            return false; // Return false if the connection fails
+            return false; 
         }
 
-        // Your SQL query to retrieve items (adjust column names as needed)
+        //  sql query to retrieve items 
         $sql = "SELECT item_ID, name, description FROM Item";
 
-        $result = $conn->query($sql); // Execute the query
+        $result = $conn->query($sql); 
 
         if (!$result) {
-            return false; // Return false if the query fails
+            return false; 
         }
 
-        return $result; // Return the query result (should be a MySQLi result object)
+        return $result; // return result
     }
 }
 
-// Function to get cart items
+// get cart items
 if (!function_exists('getCartItems')) {
     function getCartItems() {
         if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
@@ -35,11 +34,9 @@ if (!function_exists('getCartItems')) {
     }
 }
 
-// Function to add an item to the cart
+// add an item to the cart
 if (!function_exists('addToCart')) {
     function addToCart($itemId) {
-        // Your logic to add the item to the cart (using session or database)
-        // Example: adding $itemId to the $_SESSION['cart'] array
         $_SESSION['cart'][] = $itemId;
     }
 }
@@ -47,47 +44,47 @@ if (!function_exists('addToCart')) {
 if (!function_exists('removeFromCart')) {
     function removeFromCart($itemId) {
         if (!isset($_SESSION['cart']) || empty($_SESSION['cart'])) {
-            return; // Nothing to remove if the cart is empty
+            return; // nothing to remove if the cart is empty
         }
 
-        // Find the index of the item in the cart
+        // locate the index of the item in the cart
         $index = array_search($itemId, $_SESSION['cart']);
 
         if ($index !== false) {
-            // Remove the item from the cart array
+            //remove the item from the cart 
             unset($_SESSION['cart'][$index]);
 
-            // Reset array keys to avoid gaps
+            // reset array keys 
             $_SESSION['cart'] = array_values($_SESSION['cart']);
         }
     }
 }
 
-// Function to get item details based on item ID
+//get item details based on item ID
 if (!function_exists('getItemDetails')) {
     function getItemDetails($itemID) {
-        $conn = get_db_connection(); // Get the database connection
+        $conn = get_db_connection(); // get the database connection
 
         if ($conn === false) {
-            return false; // Return false if the connection fails
+            return false; 
         }
 
-        // Your SQL query to retrieve item details based on $itemID
+        // sql query to retrieve item details based on $itemID
         $sql = "SELECT item_ID, name, description FROM Item WHERE item_ID = $itemID";
 
-        $result = $conn->query($sql); // Execute the query
+        $result = $conn->query($sql); 
 
         if (!$result || $result->num_rows == 0) {
-            return false; // Return false if the query fails or no item found
+            return false; 
         }
 
-        $itemDetails = $result->fetch_assoc(); // Fetch item details
+        $itemDetails = $result->fetch_assoc(); // get item details
 
-        return $itemDetails; // Return the item details as an associative array
+        return $itemDetails; // return the item details as an associative array
     }
 }
 
-// Function to save quote information to the database
+// save quote information to the database
 if (!function_exists('saveQuote')) {
     function saveQuote($id, $name, $address, $creditCard) {
         $conn = get_db_connection(); // Get the database connection
@@ -96,44 +93,44 @@ if (!function_exists('saveQuote')) {
             return false; // Return false if the connection fails
         }
 
-        // Sanitize inputs (you should perform proper validation and sanitation)
+        // clean inputs
         $id = mysqli_real_escape_string($conn, $id);
         $name = mysqli_real_escape_string($conn, $name);
         $address = mysqli_real_escape_string($conn, $address);
         $creditCard = mysqli_real_escape_string($conn, $creditCard);
 
-        // Insert into the "Quote" table
+        // insert into the "Quote" table
         $query = "INSERT INTO Quote (id, name, address, credit_card) VALUES ('$id', '$name', '$address', '$creditCard')";
 
         if (mysqli_query($conn, $query)) {
-            // Return the ID of the inserted record
+            // return the ID of the inserted record
             return mysqli_insert_id($conn);
         } else {
-            // Return false if the insertion fails
+          
             return false;
         }
     }
 }
 
-// Function to save cart items associated with a quote
+// save cart items associated with a quote
 if (!function_exists('saveCartQuote')) {
     function saveCartQuote($quoteId, $cartItems) {
-        $conn = get_db_connection(); // Get the database connection
+        $conn = get_db_connection(); // get the database connection
 
         if ($conn === false) {
-            return false; // Return false if the connection fails
+            return false; 
         }
 
-        // Iterate through cart items and insert them into the "Cart_Quote" table
+        // go through cart items and insert them into the "Cart_Quote" table
         foreach ($cartItems as $itemID) {
             $itemID = mysqli_real_escape_string($conn, $itemID);
             $query = "INSERT INTO Cart_Quote (quote_ID, item_ID) VALUES ('$quoteId', '$itemID')";
             mysqli_query($conn, $query);
         }
 
-        return true; // Return true on success
+        return true; 
     }
 }
 
-// Other functions...
+
 ?>
