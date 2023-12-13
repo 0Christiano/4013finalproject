082511aa5147d7e674c19eba5743bf0189fa7a42
robi0161 <142837563+robi0161@users.noreply.github.com>
2023-12-13 13:52:03 -0600

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -9,8 +9,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit_checkout'])) {
     $address = $_POST['address'];
     $creditCard = $_POST['credit_card'];
 
-  
-
     // concatenate user information into a single string
     $customerInfo = "ID: $id, Name: $name, Address: $address, Credit Card: $creditCard";
 
@@ -29,15 +27,72 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit_checkout'])) {
         // clear cart after checkout 
         clearCart();
 
-        //redirect to index(home)
+        // Redirect to index (home)
         header('Location: index.php');
         exit();
     } else {
-        echo "<p>Failed to save user information into the statement table.</p>";
+        echo "<p class='text-danger'>Failed to save user information into the statement table.</p>";
     }
 } else {
-    // redirect to the checkout form 
+    // Redirect to the checkout form 
     header('Location: checkout-form.php');
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout Confirmation</title>
+    <!-- Add your custom stylesheets or link to a CSS file here -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+            padding: 20px;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: auto;
+        }
+
+        h2 {
+            color: #007bff;
+        }
+
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+
+        .btn {
+            background-color: #007bff;
+            color: #fff;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+        <h2>Checkout Confirmation</h2>
+
+        <p>Thank you for your purchase! Your order has been successfully processed.</p>
+
+        <!-- Display customer information if needed -->
+        <p>Customer Information: <?php echo htmlspecialchars($customerInfo); ?></p>
+
+        <a href="index.php" class="btn btn-primary">Back to Home</a>
+    </div>
+
+    <!-- Bootstrap JS and dependencies (add them at the end of the body for better performance) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
