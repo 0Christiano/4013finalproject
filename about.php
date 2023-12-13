0a692671de2436886d904d3d29c6ed1510e3dfbf
Christian <142837529+0Christiano@users.noreply.github.com>
2023-12-13 13:50:37 -0600

    Update about.php

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - CJ's MarketPlace</title>
-    <!-- Add your custom stylesheets or link to a CSS file here -->
+ 
     <style>
         body {
             font-family: 'Arial', sans-serif;
